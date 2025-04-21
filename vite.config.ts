0d5e6ad49e571c02b5f6266a264cca4cd7896495
neeleshgadi <neeleshgadi@gmail.com>
2025-04-21 21:05:20 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-bento-grid/', // Replace with your GitHub repo name
+  base: "/Portfolio/", // Replace with your GitHub repo name
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
